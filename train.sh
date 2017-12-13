499fbcac12c2aafe36cd4a247071b90c4f86a097
dlmacedo <dlm@cin.ufpe.br>
2017-12-13 05:32:34 +0000

    RECOVERING CNN+RNN BEST OF ALL TIMESgit status!

--- train.sh
+++ train.sh
@@ -1,19 +1,7 @@
-#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1 python train.py \
-#--train_manifest data/mit_train_speaker_identification.csv --val_manifest data/mit_val_speaker_identification.csv --cuda --batch_size 20 \
-#--loss_type full --learning-rate 0.10 --epochs 300 --cnn_features 32 --kernel 3 --stride 1 \
-#--sample_proportion 0.8 --first_layer_type CONV --mfcc false | tee spect_full_lr0.10-0.99_arch-conv32-64-128.log
-
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1 python train.py \
 --train_manifest data/mit_train_speaker_identification.csv --val_manifest data/mit_val_speaker_identification.csv --cuda --batch_size 20 \
---loss_type reg --learning-rate 0.05 --epochs 100 --cnn_features 600 --kernel 11 --stride 3 --hidden_layers 1 --hidden_size 600 \
---sample_proportion 0.8 --crop_begin 300 --crop_end 300 --first_layer_type CONV --mfcc false #| tee spect_full_lr0.05-0.99_arch-conv32-64-128.log
-
-#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1 python train.py \
-#--train_manifest data/mit_train_speaker_identification.csv --val_manifest data/mit_val_speaker_identification.csv --cuda --batch_size 20 \
-#--loss_type full --learning-rate 0.01 --epochs 300 --cnn_features 32 --kernel 3 --stride 1 \
-#--sample_proportion 0.8 --first_layer_type CONV --mfcc false | tee spect_full_lr0.01-0.99_arch-conv32-64-128.log
-
-# --hidden_layers 2 --hidden_size 600
+--loss_type reg --learning-rate 0.05 --epochs 300 --cnn_features 600 --kernel 11 --stride 3 --hidden_layers 1 --hidden_size 600 \
+--sample_proportion 0.8 --crop_begin 40 --crop_end 40 --first_layer_type CONV --mfcc false #| tee spect_reg_lr0.05-0.99_train-prop0.8_test-full_arch-cnn600x11x3-rnn1x600.log
 
 #############################################################################
 
